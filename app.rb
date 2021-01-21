b9c4f7a3ee5a087076e05a9e4c577f4d4ffb0783
chrislemus <icrislemus@gmail.com>
2021-01-21 07:46:13 -0500

    commit tree 2a41f0534914336fd1689ca2bac2a4a2c045c6ce

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get('/') {
+    erb :index
+  }
 
+  get('/new') {
+    erb :create_puppy
+  }
+
+  post('/puppy') {
+    @puppy = Puppy.new(params['name'], params['breed'], params['age'])
+    puts @puppy
+    erb :display_puppy
+  }
 end
